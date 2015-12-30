d4de3faaacdb04dcd9b372a4d4f47e7d2b164b73
Jaime García <elgambitero@gmail.com>
2015-12-30 22:19:48 +0100

    Attempt to make a ManageSamples context

--- app/src/main/java/io/github/elgambitero/microspot_android/ManageSamples.java
+++ app/src/main/java/io/github/elgambitero/microspot_android/ManageSamples.java
@@ -1,7 +1,15 @@
 package io.github.elgambitero.microspot_android;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by elgambitero on 30/12/15.
  */
-public class ManageSamples {
+public class ManageSamples extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.managesamples);
+    }
 }
