00ed7bfa482a0813ca60b023fa724e6d304a1bda
Jaime García <elgambitero@gmail.com>
2015-12-30 20:55:25 +0100

    Make layout for the new scan activity

---
+++ app/src/main/java/io/github/elgambitero/microspot_android/NewScan.java
@@ -0,0 +1,16 @@
+package io.github.elgambitero.microspot_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by elgambitero on 30/12/15.
+ */
+public class NewScan extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newscan);
+    }
+}
