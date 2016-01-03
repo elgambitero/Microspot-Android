4e0d2fe062d484064c4de43bfa747675263f8cd9
Jaime García <elgambitero@gmail.com>
2016-01-03 18:40:44 +0100

    Make first attempt at implementing the Calibrate class camera view

--- app/src/main/java/io/github/elgambitero/microspot_android/Calibrate.java
+++ app/src/main/java/io/github/elgambitero/microspot_android/Calibrate.java
@@ -1,12 +1,22 @@
 package io.github.elgambitero.microspot_android;
 
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
 
 /**
  * Created by elgambitero on 02/01/16.
@@ -15,28 +25,107 @@ public class Calibrate extends AppCompatActivity implements View.OnClickListener
 
     Button topLeft,topRight, bottomLeft, bottomRight;
     Toolbar calibToolbar;
+    FrameLayout framePreview;
+    CameraPreview preview;
+    ImageView nocamview;
+    Camera cam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibrate);
-        initializeVars();
-        setSupportActionBar(calibToolbar);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+
+
     }
 
-    private void initializeVars(){
+    @Override
+    protected void onResume(){
+        super.onResume();
+        initializeLayout();
+
+        cam = initializeCamera();
+        if(cam!=null){
+
+            preview = new CameraPreview(this,cam);
+            framePreview.addView(preview);
+        }else{
+
+            nocamview.setImageResource(R.drawable.errorsign);
+            framePreview.addView(nocamview);
+        }
+
+    }
+
+    private void initializeLayout(){
         topLeft = (Button)findViewById(R.id.TopLeft);
         topRight = (Button)findViewById(R.id.TopRight);
         bottomLeft = (Button)findViewById(R.id.BottomLeft);
         bottomRight = (Button)findViewById(R.id.BottomRight);
         calibToolbar = (Toolbar)findViewById(R.id.calibtoolbar);
+        framePreview = (FrameLayout)findViewById(R.id.camera_preview);
+        nocamview = new ImageView(this);
+
         topLeft.setOnClickListener(this);
         topRight.setOnClickListener(this);
         bottomLeft.setOnClickListener(this);
         bottomRight.setOnClickListener(this);
 
+        setSupportActionBar(calibToolbar);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+
+    }
+
+
+
+
+    private Camera initializeCamera(){
+        if(checkCameraHardware(this)){
+            Camera cam;
+            int cameraId = -1;
+            int numberOfCameras = Camera.getNumberOfCameras();
+            for (int i = 0; i < numberOfCameras; i++) {
+                Camera.CameraInfo info = new Camera.CameraInfo();
+                Camera.getCameraInfo(i, info);
+                if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                    cameraId = i;
+                    break;
+                }
+            }
+            cam = getCameraInstance(cameraId);
+            if (cam != null){
+
+            }else{
+                Log.println(1, "Err", "NO CAMERA INSTANCED");
+            }
+            return cam;
+        }else{
+            return null;
+        }
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(int i){
+        Camera c = null;
+        try {
+            c = Camera.open();
+            // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 
 
@@ -48,4 +137,5 @@ public class Calibrate extends AppCompatActivity implements View.OnClickListener
                         Snackbar.LENGTH_LONG).setAction("Action", null).show();
         }
     }
+
 }
--- app/src/main/java/io/github/elgambitero/microspot_android/Splash.java
+++ app/src/main/java/io/github/elgambitero/microspot_android/Splash.java
@@ -3,6 +3,7 @@ package io.github.elgambitero.microspot_android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.WindowManager;
 
 /**
@@ -10,12 +11,16 @@ import android.view.WindowManager;
  */
 public class Splash extends AppCompatActivity {
 
+    Toolbar splashtoolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        splashtoolbar = (Toolbar)findViewById(R.id.splashtoolbar);
+        setSupportActionBar(splashtoolbar);
         Thread timer = new Thread(){
             public void run(){
                 try{
