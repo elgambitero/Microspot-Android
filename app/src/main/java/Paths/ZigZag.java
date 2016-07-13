33f4b91d0f518453d1c72313c4f685c8543227e5
Jaime García Villena <garciavillena.jaime@gmail.com>
2016-07-13 13:46:25 +0200

    Add new ZigZag constructor that takes an area instead of the intervals, and returns the path along a square

--- app/src/main/java/Paths/ZigZag.java
+++ app/src/main/java/Paths/ZigZag.java
@@ -14,19 +14,20 @@ public class ZigZag{
 
         points = new double[shots[0]*shots[1]][2];
 
-        for(int j = 0; j < shots[1]; j++){
-            for(int i = 0; i < shots[0]; i++){
+        calcPoints(center,spacing,shots);
 
-                double[] coord = new double[2];
+    }
 
-                coord[0] = center[0] + isEven(j)*(i*spacing[0] - ( spacing[0]*(shots[0] - 1)/2 ) ) ;
-                coord[1] = center[1] + (j*spacing[1] - ( spacing[1]*(shots[1] - 1)/2 ) ) ;
+    public ZigZag(double[] center, double area, int[] shots){
 
-                points[j*shots[0] + i][0] = coord[0];
-                points[j*shots[0] + i][1] = coord[1];
+        points = new double[shots[0]*shots[1]][2];
 
-            }
-        }
+        double[] intervals = new double[2];
+
+        intervals[0] = Math.sqrt(area)/shots[0];
+        intervals[1] = Math.sqrt(area)/shots[1];
+
+        calcPoints(center,intervals,shots);
 
     }
 
@@ -41,6 +42,22 @@ public class ZigZag{
             return 1;
         }else{
             return -1;
+        }
+    }
+
+    private void calcPoints(double[] center, double[] spacing, int[] shots){
+        for(int j = 0; j < shots[1]; j++){
+            for(int i = 0; i < shots[0]; i++){
+
+                double[] coord = new double[2];
+
+                coord[0] = center[0] + isEven(j)*(i*spacing[0] - ( spacing[0]*(shots[0] - 1)/2 ) ) ;
+                coord[1] = center[1] - (j*spacing[1] - ( spacing[1]*(shots[1] - 1)/2 ) ) ;
+
+                points[j*shots[0] + i][0] = coord[0];
+                points[j*shots[0] + i][1] = coord[1];
+
+            }
         }
     }
 
